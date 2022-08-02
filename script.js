cd5e8e42a58ff2592f97d2ab150597277975c06b
rodrigomaruya <109206973+rodrigomaruya@users.noreply.github.com>
2022-08-02 16:30:19 +0900

    video mp4

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ var p = document.getElementById('parag')
 var h = document.getElementById('h1')
 let video = document.getElementById("video")
 
- bflor.addEventListener('click' ,clicar)
 
+bflor.addEventListener('click' ,clicar)
  function clicar(){
-   document.body.style.backgroundImage = "url('fotos/caveira2.jpg')"
+   document.body.style.background = "black"
    jo.style.display = 'block';
    bflor.style.display= "none"
-   
    fon.style.visibility = 'visible'
    p.style.visibility = 'visible'
    h.style.visibility = 'hidden'
-   
    video.autoplay = true
    video.load();
-
    document.getElementById("imagens").style.display = "none"
 }
+   
+   
+
 p.addEventListener('click',clicar2 )
 function clicar2(){
    location.reload();
